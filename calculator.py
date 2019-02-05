f464d9535849894bde937e7438c8f82e916b4712
Jason-leung123 <45101222+Jason-leung123@users.noreply.github.com>
2019-02-05 16:55:01 +0000

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -14,41 +14,135 @@ class number():
     def divide_2_num(self):
         out = self.num / self.num1
         return out
-
 import math        
 try:
     question = int(input("How many numbers do you want to use?\n:"))
 
     if question == 1:
-        print("Do you want to:\nFactorise\nSquare root\nSquare it")
+        print("To Factorise, type in = 1\nTo Square root, type in: 2\nTo Square it, type in: 3")
         choice = input("Please type in one of the items in the list\n:")
-        if choice == 'Factorise':
-            num = int(input("Type in a number:"))
+        if choice == '1':
+            num = int(input("Type in a number:")) 
             fact = [math.factorial(num) for num in range(num)]
             print(fact)
-        elif choice == 'Sqaure root':
+        elif choice == '2': 
             num = int(input("Type in a number:"))
-            print(math.sqrt(num))           
+            print(math.sqrt(num))
+        elif choice == '3':
+            num = int(input("Type in a number:"))
+            print(num ** num)
 
     if question == 2:
         output = number(int(input("Type in a number:")),int(input("Type in another number:")))
         while True:
-            s = input("If you want to add, press '+'\nIf you want to subract, press '-'\nIf you want to multiply, press '*'\nIf you want to divide, press '/'\n:")
-            if s == '+':
-                print(output.add_2_num())
-                break
-            elif s == '-':
+            s = input("If you want to add, press '1'\nIf you want to subract, press '2'\nIf you want to multiply, press '3'\nIf you want to divide, press '4'\n:")
+            if s == '1':
+                print(output.add_2_num()) # problem solved
+                out = output.add_2_num()
+                print("Do you want to include another number? Y/N")
+                answer = input(":")
+                if answer == 'Y' or 'y' or 'Yes' or 'yes':
+                    print("Type in a number")
+                    answer1 = int(input(":"))
+                    a = (number(out,answer1))
+                    p = int(input(("If you want to add, press '1'\nIf you want to subract, press '2'\nIf you want to multiply, press '3'\nIf you want to divide, press '4'\n:")))
+                    if p == 1:
+                        print(a.add_2_num())
+                        break
+                    elif p == 2:
+                        print(a.subtract_2_num())                 
+                        break
+                    elif p == 3:
+                        print(a.multiply_2_num()) 
+                        break
+                    elif p == 4:
+                        print(a.divide_2_num())
+                        break
+                    else:
+                        "This value cannot be accpeted!"
+                elif answer == 'N' or 'n' or 'No' or 'no':
+                    break
+            elif s == '2':
                 print(output.subtract_2_num())
-                break
-            elif s == '*':
-                print(output.multiply_2_num())
-                break
-            elif s == '/':
-                print(output.divide_2_num())
-                break
-            else:
-                print("-----------------------------------------------\nThis value cannot be accepted. Please try agian\n-----------------------------------------------")
-                continue
+                out = output.subtract_2_num()
+                print("Do you want to include another number? Y/N")
+                answer = input(":")
+                if answer == 'Y' or 'y' or 'Yes' or 'yes':
+                    print("Type in a number")
+                    answer1 = int(input(":"))
+                    p = int(input("If you want to add, press '1'\nIf you want to subract, press '2'\nIf you want to multiply, press '3'\nIf you want to divide, press '4'\n:"))
+                    a = number(out, answer1)
+                    if p == 1:
+                        print(a.add_2_num())
+                        break
+                    elif p == 2:
+                        print(a.subtract_2_num())
+                        break
+                    elif p == 3:
+                        print(a.multiply_2_num())
+                        break
+                    elif p == 4:
+                        print(a.divide_2_num())
+                        break
+                elif answer == 'N' or 'n' or 'No' or 'no':
+                    break
 
+                     
+            elif s == '3':
+                print(output.multiply_2_num())
+                out = output.multiply_2_num()
+                print("Do you want to include another number? Y/N")
+                answer = input(":")
+                if answer == 'Y' or 'y' or ' Yes' or 'yes':
+                    print("Type in a number")
+                    answer1 = int (input(":"))
+                    p = int(input("If you want to add, press '1'\nIf you want to subract, press '2'\nIf you want to multiply, press '3'\nIf you want to divide, press '4'\n:"))
+                    a = number(out, answer1)
+                    if p == 1:
+                        print(a.add_2_num())
+                        break
+                    elif p == 2: 
+                        print(a.subtract_2_num())
+                        break
+                    elif p == 3:
+                        print(a.multiply_2_num())
+                        break
+                    elif p == 4:
+                        print(a.divide_2_num())
+                        break
+                elif answer == 'N' or 'n' or 'no' or 'No':
+                    break
+                break
+            elif s == '4':
+                print(output.divide_2_num())
+                out = output.divide_2_num()
+                print("Do you want to include another number? Y/N")
+                answer = input(":")
+                if answer == 'Y' or 'y' or 'Yes' or 'yes':
+                    print("Type in a number")
+                    answer1 = int(input(":"))
+                    p = int(input("If you want to add, press '1'\nIf you want to subract, press '2'\nIf you want to multiply, press '3'\nIf you want to divide, press '4'\n:"))
+                    a = number(out, answer1)
+                    if p == 1:
+                        print(a.add_2_num())
+                        break
+                    elif p == 2:
+                        print(a.subtract_2_num())
+                        break
+                    elif p == 3:
+                        print(a.multiply_2_num())
+                        break
+                    elif p == 4:
+                        print(a.divide_2_num())
+                        break
+                elif answer == 'N' or 'n' or 'no' or 'No':
+                    break
+            else:
+                print("""
+                -----------------------------------------------
+                This value cannot be accepted. Please try agian
+                -----------------------------------------------""")
+                continue
+                
 except:
     print("This value cannot be accepted")
